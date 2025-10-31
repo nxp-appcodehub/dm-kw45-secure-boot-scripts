64331018881f1a28460405942d9dbd0d70a9e1df
WBI\nxf40158 <xing.chang@nxp.com>
2025-10-31 08:34:22 +0800

    add read bin script

---
+++ script/13.read_bin.cmd
@@ -0,0 +1,26 @@
+@rem Copyright 2021-2025 NXP
+@rem All rights reserved.
+
+@rem SPDX-License-Identifier: BSD-3-Clause
+
+@echo off
+:select_comport
+reg query "HKEY_LOCAL_MACHINE\HARDWARE\DEVICEMAP\SERIALCOMM" /v  \Device\*
+
+set /P comport=Please select comport name: 
+
+if "%comport%"=="" (
+	goto quit
+) else if /i not "%comport:~0,3%"=="com" (
+	set comport=
+	goto select_comport
+)
+@echo on
+
+::
+blhost -p %comport%,460800  read-memory 0x00 0x100000 .\readback.bin
+
+
+pause
+
+:quit
