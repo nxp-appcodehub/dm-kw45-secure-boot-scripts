7aac82548a8907ec9731dd0b2c99c968b1748411
WBI\nxf40158 <xing.chang@nxp.com>
2025-10-31 08:38:12 +0800

    add parse sb3 script

---
+++ script/14.parse_sb3.cmd
@@ -0,0 +1,8 @@
+@rem Copyright 2021-2024 NXP
+@rem All rights reserved.
+
+@rem SPDX-License-Identifier: BSD-3-Clause
+
+nxpimage sb31 parse -f kw45b41z8 -b "..\workspace\bootable_images\app_maincore.sb3" -k 7aa7ef9813b3561257b8837dab26225301df3511217f2733c71dadcd447722d1 -a 3 -o  "..\workspace\parse_images
+
+pause
